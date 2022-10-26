b6692e1e293700e4b7797c184f48fa5572edfb20
Paulo da Silva <kashpaulo@gmail.com>
2022-10-25 23:23:01 -0300

    iniciando o portifólio

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var rellax = new Rellax('.rellax');
+
+typeWriter = (elemento) => {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra , 75 * i);//tempo
+    })
+}
+//letras digitadas
+const subtitulo = document.querySelector('h2');
+typeWriter(subtitulo);
+typeWriter(document.querySelector('h1'));
+
+//mudar cor da navbar ao rolar
+const menu = document.querySelector('header');
+
+activedScroll = () => {
+    //olhar a classe e adicionar uma nova
+    menu.classList.toggle('ativo', scrollY > 0);
+}
+
+window.addEventListener('scroll', activedScroll)
